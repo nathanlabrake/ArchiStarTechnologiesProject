42bcfa255c7ef89b79246a78b843d161b1c774b0
Ubuntu <nathaniallabrak@nathaniallabrak.e3pcaoxouz4u3gvdggzeklrwtc.gx.internal.cloudapp.net>
2021-03-02 07:12:34 +0000

    Added in a test console log

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello world! This is the start of the project.");
+		System.out.println("Printing another line for a test.");
 	}
 }
